4db553b9d1ea757aa8b042a8f228cd3534536a98
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2023-08-04 20:14:38 +0530

    Extract Playground Editor

--- src/app-container.tsx
+++ src/app-container.tsx
@@ -1,63 +1,12 @@
 import React from 'react';
-import { EditorComposer, Editor, Divider } from '../src/components/playground';
-import ToolbarPlugin from '../src/components/playground/plugins/ToolbarPlugin/ToolbarPlugin';
-
-import {
-  AlignDropdown,
-  BackgroundColorPicker,
-  BlockFormatDropdown,
-  BoldButton,
-  CodeFormatButton,
-  CodeLanguageDropdown,
-  FloatingLinkEditor,
-  FontFamilyDropdown,
-  FontSizeDropdown,
-  InsertDropdown,
-  InsertLinkButton,
-  ItalicButton,
-  RedoButton,
-  TextColorPicker,
-  TextFormatDropdown,
-  UnderlineButton,
-  UndoButton,
-} from '../src/components/playground/plugins/ToolbarPlugin/components';
-import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
-
-const initialState = () => {
-  const paragraph = $createParagraphNode();
-  const text = $createTextNode('Hello World!');
-  paragraph.append(text);
-  const root = $getRoot();
-  root.append(paragraph);
-  root.selectEnd();
-};
 
 import './app.css';
+import { PlaygroundEditor } from './components';
 
 function AppContainer(): React.ReactElement {
   return (
     <div className={'app-container'}>
-      <EditorComposer initialEditorState={initialState}>
-        <Editor>
-          <ToolbarPlugin>
-            <FontFamilyDropdown />
-            <FontSizeDropdown />
-            <Divider />
-            <BoldButton />
-            <ItalicButton />
-            <UnderlineButton />
-            <CodeFormatButton />
-            <InsertLinkButton />
-            <TextColorPicker />
-            <BackgroundColorPicker />
-            <TextFormatDropdown />
-            <Divider />
-            <InsertDropdown />
-            <Divider />
-            <AlignDropdown />
-          </ToolbarPlugin>
-        </Editor>
-      </EditorComposer>
+      <PlaygroundEditor />
     </div>
   );
 }
---
+++ src/components/playground-editor/playground-editor.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  AlignDropdown,
+  BackgroundColorPicker,
+  BoldButton,
+  CodeFormatButton,
+  Divider,
+  Editor,
+  EditorComposer,
+  FontFamilyDropdown,
+  FontSizeDropdown,
+  InsertDropdown,
+  InsertLinkButton,
+  ItalicButton,
+  TextColorPicker,
+  TextFormatDropdown,
+  UnderlineButton,
+} from '../playground';
+import ToolbarPlugin from '../playground/plugins/ToolbarPlugin/ToolbarPlugin';
+import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical';
+
+const initialState = () => {
+  const paragraph = $createParagraphNode();
+  const text = $createTextNode('Hello World!');
+  paragraph.append(text);
+  const root = $getRoot();
+  root.append(paragraph);
+  root.selectEnd();
+};
+
+export const PlaygroundEditor = (): React.ReactElement => {
+  return (
+    <div className={'app-container'}>
+      <EditorComposer initialEditorState={initialState}>
+        <Editor>
+          <ToolbarPlugin>
+            <FontFamilyDropdown />
+            <FontSizeDropdown />
+            <Divider />
+            <BoldButton />
+            <ItalicButton />
+            <UnderlineButton />
+            <CodeFormatButton />
+            <InsertLinkButton />
+            <TextColorPicker />
+            <BackgroundColorPicker />
+            <TextFormatDropdown />
+            <Divider />
+            <InsertDropdown />
+            <Divider />
+            <AlignDropdown />
+          </ToolbarPlugin>
+        </Editor>
+      </EditorComposer>
+    </div>
+  );
+};
